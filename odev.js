a4d8aa8978d2ed9882db87aceecf41903aede692
seydaozdemir1603 <seydaozdemir1603@gmail.com>
2022-06-02 19:24:48 +0300

    new commit

---
+++ odev.js
@@ -0,0 +1,150 @@
+//ÖDEV
+//Örnek: 1 ile kullanıcının vereceği (prompt) bitiş sayısına göre--DONE
+//kullancı: 5 verdi diyelim 1<=X<=5--DONE
+//1.adım: kaç tane sayı var ?--DONE
+//2.adım: sayı toplamları var ?--DONE
+//3.adım: kaç tane tek  sayı var ?--DONE
+//4.adım: kaç tane çift  sayı var ?--DONE
+//Eğer verilen sayılarda 7 sayısı varsa bunu eklemesin (continue)--DONE
+//Eğer bitiş sayısı 100 fazla ise çalışmasın (break)--????????
+//Eğer kullanıcı başlangıç sayıdan küçük girerse uyaralım başlangıçtan büyük girmesini isteyelim--????????
+//Eğer kullanıcı secret-key girerse yani 44 sayısını girerse program çalışmadan dursun --DONE
+
+
+var x = prompt("bir sayı girin: ");
+
+if (x == 44) {
+    alert("bu sayıyı giremezsiniz")
+    x = null;
+}
+
+document.writeln("girdiğiniz sayı: " + x + "<br>");
+
+function f1(x) {
+    document.writeln("1'den girdiğiniz sayıya kadar olan aralık: ");
+    for(var i = 1; i <= x; i++){
+        document.writeln(i);
+    }
+    document.writeln("<br>");
+}
+ 
+f1(x);
+
+function f2(x) {
+    document.writeln("kaç tane sayı var?: ");
+    count = 0;
+    for(var i = 1; i <= x; i++){
+        count++;
+        }
+        document.writeln(count);
+        document.writeln("<br>");
+}
+
+f2(x);
+
+
+function f3(x) {
+    document.writeln("sayı toplamları nedir?: ");
+    sum = 0;
+    for(var i = 1; i <= x; i++){
+        sum = sum + i;
+        }
+        document.writeln(sum);
+        document.writeln("<br>");
+}
+
+f3(x);
+
+function f4(x) {
+    document.writeln("tek sayılar: ");
+    for(var i = 1; i <= x; i++){
+        if(i%2 ==! 0){
+            document.writeln(i);
+        }
+     }
+        document.writeln("<br>");
+}
+
+f4(x);
+
+function f4a(x) {
+    document.writeln("kaç tane tek sayı var?: ");
+    count = 0;
+    for(var i = 1; i <= x; i++){
+        if(i%2 ==! 0){
+            count++;
+        }
+     }
+        document.writeln(count);
+        document.writeln("<br>");
+}
+
+f4a(x);
+
+function f4b(x) {
+    document.writeln("tek sayılar toplamı: ");
+    sum = 0;
+    for(var i = 1; i <= x; i++){
+        if(i%2 ==! 0){
+            sum = sum + i;
+        }
+     }
+        document.writeln(sum);
+        document.writeln("<br>");
+}
+
+f4b(x);
+
+function f5(x) {
+    document.writeln("çift sayılar: ");
+    for(var i = 1; i <= x; i++){
+        if(i%2 == 0){
+            document.writeln(i);
+        }
+     }
+        document.writeln("<br>");
+}
+
+f5(x);
+
+function f5a(x) {
+    document.writeln("kaç tane çift sayı var?: ");
+    count = 0;
+    for(var i = 1; i <= x; i++){
+        if(i%2 == 0){
+            count++;
+        }
+     }
+        document.writeln(count);
+        document.writeln("<br>");
+}
+
+f5a(x);
+
+function f5b(x) {
+    document.writeln("çift sayılar toplamı: ");
+    sum = 0;
+    for(var i = 1; i <= x; i++){
+        if(i%2 == 0){
+            sum = sum + i;
+        }
+     }
+        document.writeln(sum);
+        document.writeln("<br>");
+}
+
+f5b(x);
+
+
+function f6(x) {
+    document.writeln("İçlerinde 7 sayısı varsa ekleme: ");
+    for(var i = 1; i <= x; i++){
+        if(i !== 7){
+            document.writeln(i);
+        }
+    }
+    document.writeln("<br>");
+}
+f6(x);
+
+
